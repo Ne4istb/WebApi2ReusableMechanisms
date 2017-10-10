abfc866c1963d0e47d0e56922839d840d7c67276
Andriy Nechitailov <andriy.nechitailov@wolterskluwer.com>
2017-10-10 13:45:59 +0300

    Initial commit

---
+++ WebApi2ReusableMechanisms.Tests/Controllers/HomeControllerTest.cs
@@ -0,0 +1,25 @@
+﻿using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WebApi2ReusableMechanisms;
+using WebApi2ReusableMechanisms.Controllers;
+
+namespace WebApi2ReusableMechanisms.Tests.Controllers
+{
+	[TestClass]
+	public class HomeControllerTest
+	{
+		[TestMethod]
+		public void Index()
+		{
+			// Arrange
+			HomeController controller = new HomeController();
+
+			// Act
+			ViewResult result = controller.Index() as ViewResult;
+
+			// Assert
+			Assert.IsNotNull(result);
+			Assert.AreEqual("Home Page", result.ViewBag.Title);
+		}
+	}
+}

79a42c16e4643fb2752b4d23e434206a07058ff8
Andriy Nechitailov <andriy.nechitailov@wolterskluwer.com>
2017-10-11 09:35:50 +0300

    Removed redundant autogenerated code

--- WebApi2ReusableMechanisms/Global.asax.cs
+++ WebApi2ReusableMechanisms/Global.asax.cs
@@ -16,8 +16,6 @@ namespace WebApi2ReusableMechanisms
 			AreaRegistration.RegisterAllAreas();
 			GlobalConfiguration.Configure(WebApiConfig.Register);
 			FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
-			RouteConfig.RegisterRoutes(RouteTable.Routes);
-			BundleConfig.RegisterBundles(BundleTable.Bundles);
 		}
 	}
 }
